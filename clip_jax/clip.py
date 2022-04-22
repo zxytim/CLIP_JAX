445fc979f749ff8bd9a98523b9053bf56237907f
nshepperd <nshepperd@gmail.com>
2022-04-22 10:45:46 +1000

    Add ViT-L/14. (#4)

--- clip_jax/clip.py
+++ clip_jax/clip.py
@@ -25,6 +25,7 @@ _MODELS = {
     # "RN50": "https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt",
     "ViT-B/32": "https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
     "ViT-B/16": "https://openaipublic.azureedge.net/clip/models/5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt",
+    "ViT-L/14": "https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt",
 }
 
 
--- tests/test_consistency.py
+++ tests/test_consistency.py
@@ -28,3 +28,4 @@ def test_model(model_name):
 
 test_model("ViT-B/32")
 test_model("ViT-B/16")
+test_model("ViT-L/14")
